86805579ce2419e03a52c961b979597770b3dde7
AchinMrinal <mrinal15.am@gmail.com>
2020-08-07 23:08:28 +0530

    DB connected and unhandlerejection added

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 const express = require('express');
 const dotenv = require('dotenv');
-// Routes File
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 const bootcamps = require('./routes/bootcamps');
+
+
 // Load env vars
 dotenv.config({path: './config/config.env'});
 
+// connect to database
+connectDB();
+
 const app = express();
+
+// Dev logs Info
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
 // Mount Router
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT;
 
-app.listen(
+const server = app.listen(
     PORT,
     console.log(`Server running on ${process.env.NODE_ENV} mode on ${PORT}`)
 );
+
+// Handle unhandle Promise rejection
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    server.close(()=> process.exit(1));
+});
