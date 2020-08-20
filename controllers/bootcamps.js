c218b20aa1582baa5f498ced55dc022aba150dae
AchinMrinal <mrinal15.am@gmail.com>
2020-08-20 17:49:13 +0530

    error class

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const ErrorResponse = require('../utils/errorResponse');
 const Bootcamp = require('../models/Bootcamp');
 
 // @desc        Get All Bootcamps
@@ -28,14 +29,19 @@ exports.getBootcamp = async (req, res, next) => {
     try {
         const bootcamp = await Bootcamp.findById(req.params.id);
 
-        res.status(400).json({
+        if(!bootcamp) {
+            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+        }
+
+        res.status(200).json({
             success: true,
             data: bootcamp
         });
     } catch (err) {
-        res.status(200).json({
-            success: false
-        });
+        // res.status(400).json({
+        //     success: false
+        // });
+        next(err);
     }
 }
 
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
 const connectDB = require('./config/db');
+const errorHandler = require('./middleware/error');
 const bootcamps = require('./routes/bootcamps');
 
 
@@ -24,6 +25,9 @@ if(process.env.NODE_ENV === 'development'){
 
 // Mount Router
 app.use('/api/v1/bootcamps', bootcamps);
+
+// Use Middleware error handler
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 
